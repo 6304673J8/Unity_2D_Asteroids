71b389e2767c326521854580387dfb8519438aa9
Georghe Julca <petergeorghe.julca@enti.cat>
2021-02-02 13:51:14 +0100

    Scene Manager
    Developing Load Screen System

---
+++ 2D_Asteroids/Assets/Scripts/LoadManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public static class LoadManager
+{
+    public enum Scene
+    {
+        TitleScene,
+        Loading,
+        GameScene,
+        WonScene,
+        LostScene,
+    }
+    private static Action onLoader;
+
+    public static void Load(Scene scene)
+    {
+        onLoader = () =>
+        {
+            SceneManager.LoadScene(scene.ToString());
+        };
+        SceneManager.LoadScene(Scene.Loading.ToString());
+    }
+    public static void Loader()
+    {
+        //Triggers after the first update
+        //Executes onLoader which will load the desired scene
+        if (onLoader != null)
+        {
+            onLoader();
+            onLoader = null;
+        }
+    }
+}

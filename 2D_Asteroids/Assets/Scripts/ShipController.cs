c5ffdcc8ed6389592937b5ab9816647e37c4aef6
Georghe Julca <petergeorghe.julca@enti.cat>
2021-02-14 20:11:46 +0100

    Ship Movement + Toroidal Behaviour
    Working on Bullet Instantiate

--- 2D_Asteroids/Assets/Scripts/ShipController.cs
+++ 2D_Asteroids/Assets/Scripts/ShipController.cs
@@ -4,8 +4,25 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
+    
+    //Movement
+    public float impulse = 40;
+    public float maxImpulse = 120;
+    public float drag = 1;
+    public float angularSpeed;
+    //Bullets
+    public float bulletPosition;
+    public GameObject bulletPrefab;
+
+    //Gameflow
     public int characterHealth = 10;
+
     private bool isDead = false;
+
+    private Rigidbody2D shipRb;
+
+    private float horizontal;
+    private bool shooting;
 
     private Animator animator;
     private int moveParamID;
@@ -13,11 +30,18 @@ public class ShipController : MonoBehaviour
     private int turnRightParamID;
     private int shootParamID;
     private int dieParamID;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Gameflow
+        shipRb = gameObject.GetComponent<Rigidbody2D>();
+        shipRb.drag = drag;
+        transform.position = new Vector3(0.0f, -2.0f, 0.0f);
+
+        //Animation Setup
         animator = GetComponent<Animator>();
-        moveParamID = Animator.StringToHash("isMoving");
+        moveParamID = Animator.StringToHash("isMovingStraight");
         turnLeftParamID = Animator.StringToHash("isRotatingLeft");
         turnRightParamID = Animator.StringToHash("isRotatingRight");
         shootParamID = Animator.StringToHash("doShoot");
@@ -27,6 +51,8 @@ public class ShipController : MonoBehaviour
     void Update()
     {
         bool isMoving = false;
+        horizontal = InputManager.Horizontal;
+        Rotate();
         //bool isRotatingLeft = false;
         //bool isRotatingRight = false;
         if (Input.GetKey(KeyCode.W))
@@ -54,5 +80,37 @@ public class ShipController : MonoBehaviour
             isRotatingRight = true;
         }
         animator.SetBool(moveParamID, isRotatingRight);*/
+    }
+
+    private void FixedUpdate()
+    {
+        //var acceleration = Mathf.Clamp(vertical, 0f, 1f);
+        if (Input.GetKey(KeyCode.W))
+        {
+            shipRb.AddForce(transform.up * impulse, ForceMode2D.Impulse);
+            if (shipRb.velocity.magnitude > maxImpulse)
+            {
+                shipRb.velocity = shipRb.velocity.normalized * maxImpulse;
+            }
+        }
+    }
+
+    private void Rotate () {
+        if (horizontal == 0)
+        {
+            return;
+        }
+        transform.Rotate(0, 0, -angularSpeed * horizontal * Time.deltaTime);
+    }
+
+    private void Shoot () {
+        if (shooting)
+        {
+            var pos = transform.up * bulletPosition + transform.position;
+            var bullet = Instantiate(
+                    bulletPrefab, pos, transform.rotation
+                );
+            Destroy(bullet, 5);
+        }
     }
 }
---
+++ 2D_Asteroids/Assets/Scripts/ToroidalBehaviour.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+
+public struct Borders {
+    public float upperBorder, bottomBorder, leftBorder, rightBorder;
+}
+public class ToroidalBehaviour : MonoBehaviour
+{
+    public Borders borders;
+    // Update is called once per frame
+    void Update()
+    {
+        var pos = transform.position;
+        var x = transform.position.x;
+        var y = transform.position.y;
+
+        if (x > borders.rightBorder) {
+            pos.x = borders.leftBorder;
+            transform.position = pos;
+        }
+        if (x < borders.leftBorder){
+            pos.x = borders.rightBorder;
+            transform.position = pos;
+        }
+
+        if (y > borders.upperBorder) { 
+            pos.y = borders.bottomBorder;
+            transform.position = pos;
+        }
+        if (y < borders.bottomBorder) {
+            pos.y = borders.upperBorder;
+            transform.position = pos;
+        }
+    }
+}
